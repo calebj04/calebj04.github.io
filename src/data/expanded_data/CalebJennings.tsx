8a6b137f5f53d05cd5ef98559cfeb57eca8644a9
Caleb Jennings <calebbirtch.j@gmail.com>
2025-10-23 13:59:15 -0400

    feat: Create CalebJennings component + styling

--- src/data/expanded_data/CalebJennings.tsx
+++ src/data/expanded_data/CalebJennings.tsx
@@ -1,3 +1,92 @@
+import styles from "../../styles/CalebJennings.module.css";
+
 export default function CalebJennings() {
-  return <>POOP</>;
+  const images1 = [
+    "groomsmen1.jpg",
+    "groomsmen2.jpg",
+    "groomsmen3.jpg",
+    "groomsmen4.jpg",
+    "groomsmen5.jpg",
+  ];
+
+  const images2 = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
+    "14.jpg",
+    "15.jpg",
+  ];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <div className={styles.carousel}>
+        <div className={`${styles.group} ${styles.fast}`}>
+          {images1.map((image, index) => (
+            <img
+              key={index}
+              className={styles.imgStyle}
+              src={`/images/${image}`}
+              alt={`Groomsmen Photo ${index + 1}`}
+            />
+          ))}
+        </div>
+
+        <div aria-hidden className={`${styles.group} ${styles.fast}`}>
+          {images1.map((image, index) => (
+            <img
+              key={index}
+              className={styles.imgStyle}
+              src={`/images/${image}`}
+              alt={`Groomsmen Photo ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
+
+
+      <div className={styles.carousel}>
+        <div className={`${styles.group} ${styles.slow}`}>
+          {images2.map((image, index) => (
+            <img
+              key={index}
+              className={styles.imgStyle}
+              src={`/images/${image}`}
+              alt={`Photo ${index + 1}`}
+            />
+          ))}
+        </div>
+
+        <div aria-hidden className={`${styles.group} ${styles.slow}`}>
+          {images2.map((image, index) => (
+            <img
+              key={index}
+              className={styles.imgStyle}
+              src={`/images/${image}`}
+              alt={` Photo ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
+
+    </div>
+  );
 }
