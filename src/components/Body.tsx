d6002726017968139f857b4a3af47823082b30ec
Caleb Jennings <calebbirtch.j@gmail.com>
2025-12-18 12:19:12 -0500

    feat: add daisyUI 3D card hover effect

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,15 +1,37 @@
 function Body() {
   return (
-    <div className="flex flex-col gap-5 items-center justify-center text-white">
-      <div className="bg-gray-800 flex flex-col items-center justify-center w-1/2 h-100 text-4xl font-bold mb-4 rounded-2xl">
-        Welcome to the Body Component
-      </div>
-      <div className="bg-gray-800 flex flex-col items-center justify-center w-1/2 h-100 text-4xl font-bold mb-4 rounded-2xl">
-        Welcome to the Body Component
-      </div>
-      <div className="bg-gray-800 flex flex-col items-center justify-center w-1/2 h-100 text-4xl font-bold mb-4 rounded-2xl">
-        Welcome to the Body Component
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <a href="#" className="hover-3d my-12 mx-2 cursor-pointer">
+        {/* content */}
+        <div className="card w-96 h-48 bg-black text-white ">
+          <div className="card-body ">
+            <div className="flex justify-between mb-10">
+              <div className="font-bold">Caleb Jennings</div>
+            </div>
+            <div className="text-lg mb-4 opacity-40">Software Engineer </div>
+            <div className="flex justify-between">
+              <div>
+                <div className="text-xs opacity-20">Emory University 2026</div>
+                <div>Atlanta, GA</div>
+              </div>
+              <div>
+                <div className="text-xs opacity-20">PLEASE HIRE BY</div>
+                <div>26/05</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 8 empty divs needed for the 3D effect */}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </a>
     </div>
   );
 }
