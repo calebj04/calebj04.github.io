4414640586284931167083c13e88f11ec482387b
Caleb Jennings <calebbirtch.j@gmail.com>
2025-12-20 10:40:28 -0500

    feat: make header responsive

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,37 @@
 function Card() {
   return (
     <div
-      className="flex w-1/2 aspect-[1.618] rounded-xl
+      className="flex w-1/2 min-w-100 aspect-[1.618] rounded-xl
       bg-[linear-gradient(45deg,#121521,#38476b,#b6192e,#ffc1ac)]"
     >
       <div className="grid grid-cols-[0.75fr_1fr_2fr] p-2">
         <div>
           <img className="rounded-full" src="public/images/pfp.jpg"></img>
         </div>
-        <div className="flex flex-col items-center">
-          <p className="text-white font-light text-2xl">Caleb Jennings</p>
-          <p className="text-gray-300 font-extralight text-lg">
+        <div className="flex flex-col items-center whitespace-nowrap">
+          <p className="text-white font-extralight text-[clamp(1rem,2cqw,1.5rem)] leading-none">
+            Caleb Jennings
+          </p>
+          <p className="text-gray-300 font-extralight text-[clamp(0.75rem,1.5cqw,1.125rem)] leading-tight">
             Software Engineer
           </p>
         </div>
         <div className="justify-self-end h-1/5 grid grid-cols-2 grid-rows-2 gap-2">
-          <button className="flex items-center justify-center bg-white opacity-80 text-black text-lg font-light p-2 rounded-xl">
-            Experience
+          <button className="flex items-center justify-center bg-white opacity-80 text-black font-extralight p-2 rounded-xl">
+            <span className="hidden lg:inline">Experience</span>
+            <span className="inline lg:hidden">Exp</span>
           </button>
-          <button className="flex items-center justify-center bg-white opacity-80 text-black text-lg font-light p-2 rounded-xl">
-            Projects
+          <button className="flex items-center justify-center bg-white opacity-80 text-black font-extralight p-2 rounded-xl">
+            <span className="hidden lg:inline">Projects</span>
+            <span className="inline lg:hidden">Prj</span>
           </button>
-          <button className="flex items-center justify-center bg-white opacity-80 text-black text-lg font-light p-2 rounded-xl">
-            Skills
+          <button className="flex items-center justify-center bg-white opacity-80 text-black font-extralight p-2 rounded-xl">
+            <span className="hidden lg:inline">Skills</span>
+            <span className="inline lg:hidden">Skl</span>
           </button>
-          <button className="flex items-center justify-center bg-white opacity-80 text-black text-lg font-light p-2 rounded-xl">
-            About
+          <button className="flex items-center justify-center bg-white opacity-80 text-black font-extralight p-2 rounded-xl">
+            <span className="hidden lg:inline">About</span>
+            <span className="inline lg:hidden">Abt</span>
           </button>
         </div>
       </div>
