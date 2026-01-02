2b98890249cf2358dbd88d4afa46320bdba9c9af
Caleb Jennings <calebbirtch.j@gmail.com>
2026-01-02 16:14:37 -0500

    feat: add titles to all pages

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -2,7 +2,14 @@ import type { ReactNode } from "react";
 import Card from "../Card";
 
 function About() {
-  const title: ReactNode = <></>;
+  const title: ReactNode = (
+    <div className="group flex flex-col">
+      <h1 className="text-4xl font-light tracking-tighter text-white drop-shadow-lg">
+        About
+      </h1>
+      <div className="h-1 w-12 bg-[#ffc1ac] mt-2 rounded-full opacity-80 group-hover:opacity-100 group-hover:w-21 transition-all duration-500" />
+    </div>
+  );
 
   const body: ReactNode = <></>;
 
