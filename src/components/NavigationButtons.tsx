f5f3b956769b0fa4a344d02141a89711cc34eb29
Caleb Jennings <calebbirtch.j@gmail.com>
2026-01-06 12:50:50 -0500

    removed about page :'(

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -6,27 +6,20 @@ function NavigationButtons() {
     { label: "Experience", path: "/experience" },
     { label: "Projects", path: "/projects" },
     { label: "Skills", path: "/skills" },
-    { label: "About", path: "/about" },
   ];
 
   const location = useLocation();
 
   const currentPath = location.pathname;
 
-  const home = pages[0];
-
-  const navItems = pages
-    .filter((page) => page.path !== "/") // never show Home by default
-    .map((page) => (page.path === currentPath ? home : page));
-
   return (
     <div className="grid grid-cols-2 gap-2">
-      {navItems.map((page) => (
+      {pages.map((page) => (
         <Link
           key={page.path}
           to={page.path}
           className={`border border-white/10 px-3 py-1.5 rounded-full text-xs font-semibold uppercase tracking-wide hover:bg-white hover:text-[#b6192e] hover:scale-105 transition-all duration-300 shadow-sm flex items-center justify-center ${
-            page.path === home.path
+            page.path === currentPath
               ? "bg-white text-[#b6192e]"
               : "bg-black/20 text-white/90"
           }`}
--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,14 @@ import Home from "./components/pages/Home";
 import Experience from "./components/pages/Experience";
 import Projects from "./components/pages/Projects";
 import Skills from "./components/pages/Skills";
-import About from "./components/pages/About";
+// import About from "./components/pages/About";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/experience", element: <Experience /> },
   { path: "/projects", element: <Projects /> },
   { path: "/skills", element: <Skills /> },
-  { path: "/about", element: <About /> },
+  // { path: "/about", element: <About /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
